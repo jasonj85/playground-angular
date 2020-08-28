c511e4f2bacf1d166da8b89a054c1e6090be1d8d
jasonj85 <jasonj85@gmail.com>
2020-08-28 08:15:19 +0100

    final changes to password generator app

---
+++ password-generator-app/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  password = '';
+  useLetters = false;
+  useSymbols = false;
+  useNumbers = false;
+  enableButton = false;
+  length = 0;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@£$%^&*()';
+
+    let validChars = '';
+    if (this.useLetters) validChars += letters;
+    if (this.useNumbers) validChars += numbers;
+    if (this.useSymbols) validChars += symbols;
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+}
