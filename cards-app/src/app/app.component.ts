967ce63c28bc13af7b3235437942626461c7821f
jasonj85 <jasonj85@gmail.com>
2020-08-30 08:27:16 +0100

    Basic demo of  cards using bulma styling library

--- cards-app/src/app/app.component.ts
+++ cards-app/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards-app';
+
+  posts = [
+    {
+      title: 'Beautiful tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'natureDude',
+      content: 'I saw this great tree today',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainHiFive',
+      content: 'Where are my skis!?',
+    },
+    {
+      title: 'Mountain biking!',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'lanceLegStrong',
+      content: 'What a cool place for a ride!',
+    },
+  ]
+
+
 }
